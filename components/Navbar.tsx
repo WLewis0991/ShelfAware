e41d42aed494384eb6afa22be91ac060886ccf31
wlewis0991 <wlewis1990@gmail.com>
2026-06-12 15:10:44 -0700

    feat: Home and add buttons/links added but not active

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navItems = [
+  { label: "Library", href: "/" },
+  { label: "Add New", href: "/books/new" },
+];
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
     <header className="w-full fixed z-50 bg-('b--bg-primary')">
       <div className="wrapper navbar-height py-4 flex justify-between items-center">
@@ -9,6 +19,26 @@ const Navbar = () => {
           <Image src="/assets/logo.png" alt="Logo" width={50} height={50} />
           <span className="logo-text">Shelf Aware</span>
         </Link>
+
+        <nav className="w-fit flex gap-7.5 items-center">
+          {navItems.map(({ label, href }) => {
+            const isActive =
+              pathName === href || (href !== "/" && pathName.startsWith(href));
+
+            return (
+              <Link
+                href={href}
+                key={label}
+                className={cn(
+                  "nav-link-base",
+                  isActive ? "nav-link-active" : "text-black hover:opacity-70",
+                )}
+              >
+                {label}
+              </Link>
+            );
+          })}
+        </nav>
       </div>
     </header>
   );
