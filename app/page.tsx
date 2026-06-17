67b5e9bef0c757be747361e625b7625ab13c4538
wlewis0991 <wlewis1990@gmail.com>
2026-06-17 10:21:02 -0700

    feat: Cards created to display books

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import BookCard from "@/components/BookCard";
 import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
 
 const page = () => {
   return (
     <div>
       <HeroSection />
+
+      <div className="library-hero-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
     </div>
   );
 };
