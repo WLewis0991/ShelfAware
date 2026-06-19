3124590684411d13090868b6cba9f37ded90fd28
wlewis0991 <wlewis1990@gmail.com>
2026-06-19 09:30:30 -0700

    fix: Navbar background added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   const { user } = useUser();
 
   return (
-    <header className="w-full fixed z-50 bg-('b--bg-primary')">
+    <header className="w-full fixed z-50 bg-(--bg-primary)">
       <div className="wrapper navbar-height py-4 flex justify-between items-center">
         <Link href="/" className="flex gap-0.5 items-center">
           <Image src="/assets/logo.png" alt="Logo" width={50} height={50} />
