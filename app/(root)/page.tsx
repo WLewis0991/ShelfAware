b337dfabf7d3744860c45111cac086c820e675c8
wlewis0991 <wlewis1990@gmail.com>
2026-06-25 11:51:05 -0700

    feat: Users books display on main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,18 @@
 import BookCard from "@/components/BookCard";
 import HeroSection from "@/components/HeroSection";
-import { sampleBooks } from "@/lib/constants";
+import { getAllBooks } from "@/lib/actions/book.actions";
+// import { sampleBooks } from "@/lib/constants";
 
-const page = () => {
+const page = async () => {
+  const bookResults = await getAllBooks();
+  const books = bookResults.success ? (bookResults.data ?? []) : [];
+
   return (
     <div className="wrapper container">
       <HeroSection />
 
       <div className="library-books-grid">
-        {sampleBooks.map((book) => (
+        {books.map((book) => (
           <BookCard
             key={book._id}
             title={book.title}
