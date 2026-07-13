44091eccd460f140db116d3feafdec8ab9e1773a
wlewis0991 <wlewis1990@gmail.com>
2026-07-13 11:17:53 -0700

    chore:Book db actions added

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -107,6 +107,31 @@ export const saveBookSegments = async (
   }
 };
 
+export const getBookBySlug = async (slug: string) => {
+  try {
+    await connectToDatabase();
+
+    const book = await Book.findOne({ slug }).lean();
+
+    if (!book) {
+      return {
+        success: false,
+      };
+    }
+
+    return {
+      success: true,
+      data: serializeData(book),
+    };
+  } catch (e) {
+    console.error("Error fetching book by slug:", e);
+    return {
+      success: false,
+      error: e,
+    };
+  }
+};
+
 export const getAllBooks = async () => {
   try {
     await connectToDatabase();
